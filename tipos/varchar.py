b5a50f6e0cd5f87a3451568ba09b626759136240
calexandredev <calexandredev@gmail.com>
2024-03-03 14:37:39 -0300

    working with iteration on terminal

--- tipos/varchar.py
+++ tipos/varchar.py
@@ -4,3 +4,21 @@ a = 5
 b = 5
 
 print(a + b)
+
+
+number = 48
+textGambi = 'Gambeta... '
+
+print(textGambi + str(number))
+
+
+print(f'Interpolando a {textGambi} {number}')
+
+
+PI = 3.14
+
+katchau = float(input('da o papo: '))
+
+raio = PI * pow(katchau, 2)
+
+print(raio)
